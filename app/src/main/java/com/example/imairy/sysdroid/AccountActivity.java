b40822aef082bd52035a652f75af4154e23f9ba4
adachita <adachita@systena.co.jp>
2017-06-09 10:19:03 +0900

    Activity追加 起動時ActivityをLoginActivityに

--- app/src/main/java/com/example/imairy/sysdroid/AccountActivity.java
+++ app/src/main/java/com/example/imairy/sysdroid/AccountActivity.java
@@ -3,7 +3,7 @@ package com.example.imairy.sysdroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class  extends AppCompatActivity {
+public class AccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
