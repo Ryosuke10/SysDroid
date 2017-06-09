07bcddb9f8ee1c84a03892e6e2c524a09d6020fc
adachita <adachita@systena.co.jp>
2017-06-09 10:13:52 +0900

    Activity追加 起動時ActivityをLoginActivityに

--- app/src/main/java/com/example/imairy/sysdroid/BasketActivity.java
+++ app/src/main/java/com/example/imairy/sysdroid/BasketActivity.java
@@ -3,16 +3,11 @@ package com.example.imairy.sysdroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class BasketActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //adachita Test
-        //kimura Test
-
-
-
+        setContentView(R.layout.activity_basket);
     }
 }
