46f360cc34fff1e353b824553419df8388c59fc4
adachita <adachita@systena.co.jp>
2017-06-12 15:26:52 +0900

    strings.xml変更

--- app/src/main/java/com/example/imairy/sysdroid/ConfirmActivity.java
+++ app/src/main/java/com/example/imairy/sysdroid/ConfirmActivity.java
@@ -2,6 +2,12 @@ package com.example.imairy.sysdroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static android.R.attr.animation;
 
 public class ConfirmActivity extends AppCompatActivity {
 
@@ -9,5 +15,12 @@ public class ConfirmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
+        Button Button = (Button) findViewById(R.id.button_confirm);
+        Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
